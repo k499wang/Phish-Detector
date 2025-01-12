23e0d3be08c77f27d467a40442f3ca8dce2f5371
Kevin Wang <k499wang@uwaterloo.ca>
2025-01-12 16:44:31 -0500

    Changed Init

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,7 @@ from flask_cors import CORS
 def create_app():
     app = Flask(__name__)
     
-    CORS(app, resources={r"/*": {"origins": ["http://localhost:*", "http://127.0.0.1:*"]}})
+    CORS(app)
 
     
     from .routes.url_api import url_api
